ebf39f197af91873555531ecfaba9166a6bd34ce
Declan1704 <22u1269@students.git.edu>
2025-05-17 11:12:11 +0530

    Initialised database

---
+++ backend/my-app/src/db/db.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+let prisma: ReturnType<typeof getPrismaClient>;
+
+function getPrismaClient(DATABASE_URL: string) {
+  return new PrismaClient({
+    datasourceUrl: DATABASE_URL,
+  }).$extends(withAccelerate());
+}
+
+export const getPrisma = (env: { DATABASE_URL?: string }) => {
+  if (!env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not configured in the environment");
+  }
+
+  if (!prisma) {
+    prisma = getPrismaClient(env.DATABASE_URL);
+  }
+
+  return prisma;
+};
